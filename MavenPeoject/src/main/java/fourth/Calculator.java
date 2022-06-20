1992a1b79a1e29bbf0347144a3a1a93f782662f8
Yaminimaheswari <105066881+Yaminimaheswari@users.noreply.github.com>
2022-06-20 23:06:34 +0530

    commiting a pdf file

---
+++ MavenPeoject/src/main/java/fourth/Calculator.java
@@ -0,0 +1,10 @@
+package fourth;
+
+public class Calculator {
+	 public int add(int a, int b) {
+	        return a+b;
+	    }
+
+}
+
+
---
+++ MavenPeoject/src/test/java/third/BankTest.java
@@ -0,0 +1,19 @@
+package third;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class BankTest {
+	 BankAccount cust;
+	    @Before
+	    void object_creation(){
+	       cust= new BankAccount(21,9453839);
+	    }
+	    @Test
+	    void withdraw() {
+	        assertAll(
+	                ()-> assertThrows(InsufficientBalanceException.class,() -> cust.withdraw(9000000),"It is supposed to throw Insufficient funds exception"),
+	                ()-> assertThrows(InsufficientBalanceException.class,() -> cust.withdraw(983674748),"It is supposed to throw Insufficient funds exception")
+	        );
+	    }
+}
